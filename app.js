3f119bf84774b562478e44895794c11a0ee09116
PogodaIgor <igor.pogoda2010@wp.pl>
2026-02-06 14:56:11 +0100

    a

--- app.js
+++ app.js
@@ -1,109 +1,109 @@
-var licznik = 1;
+  var licznik = 1;
 
-function generuj(id) {
-  if (id == "div1") {
-    if (document.getElementById("div1").style.backgroundColor === "gray") {
-      return;
+  function generuj(id) {
+    if (id == "div1") {
+      if (document.getElementById("div1").style.backgroundColor === "yellow") {
+        return;
+      }
+
+      document.getElementById("div1").innerHTML = licznik;
+
+      document.getElementById("div1").style.backgroundColor = "yellow";
+
+      licznik++;
     }
 
-    document.getElementById("div1").innerHTML = licznik;
+    if (id == "div2") {
+      if (document.getElementById("div2").style.backgroundColor === "yellow") {
+        return;
+      }
 
-    document.getElementById("div1").style.backgroundColor = "gray";
+      document.getElementById("div2").innerHTML = licznik;
+      document.getElementById("div2").style.backgroundColor = "yellow";
 
-    licznik++;
-  }
-
-  if (id == "div2") {
-    if (document.getElementById("div2").style.backgroundColor === "gray") {
-      return;
+      licznik++;
     }
 
-    document.getElementById("div2").innerHTML = licznik;
-    document.getElementById("div2").style.backgroundColor = "gray";
+    if (id == "div3") {
+      if (document.getElementById("div3").style.backgroundColor === "yellow") {
+        return;
+      }
 
-    licznik++;
-  }
+      document.getElementById("div3").innerHTML = licznik;
 
-  if (id == "div3") {
-    if (document.getElementById("div3").style.backgroundColor === "gray") {
-      return;
+      document.getElementById("div3").style.backgroundColor = "yellow";
+
+      licznik++;
     }
 
-    document.getElementById("div3").innerHTML = licznik;
+    if (id == "div4") {
+      if (document.getElementById("div4").style.backgroundColor === "yellow") {
+        return;
+      }
 
-    document.getElementById("div3").style.backgroundColor = "gray";
+      document.getElementById("div4").innerHTML = licznik;
+      document.getElementById("div4").style.backgroundColor = "yellow";
 
-    licznik++;
-  }
-
-  if (id == "div4") {
-    if (document.getElementById("div4").style.backgroundColor === "gray") {
-      return;
+      licznik++;
     }
 
-    document.getElementById("div4").innerHTML = licznik;
-    document.getElementById("div4").style.backgroundColor = "gray";
+    if (id == "div5") {
+      if (document.getElementById("div5").style.backgroundColor === "yellow") {
+        return;
+      }
 
-    licznik++;
-  }
+      document.getElementById("div5").innerHTML = licznik;
 
-  if (id == "div5") {
-    if (document.getElementById("div5").style.backgroundColor === "gray") {
-      return;
+      document.getElementById("div5").style.backgroundColor = "yellow";
+
+      licznik++;
     }
 
-    document.getElementById("div5").innerHTML = licznik;
+    if (id == "div6") {
+      if (document.getElementById("div6").style.backgroundColor === "yellow") {
+        return;
+      }
 
-    document.getElementById("div5").style.backgroundColor = "gray";
+      document.getElementById("div6").innerHTML = licznik;
 
-    licznik++;
-  }
+      document.getElementById("div6").style.backgroundColor = "yellow";
 
-  if (id == "div6") {
-    if (document.getElementById("div6").style.backgroundColor === "gray") {
-      return;
+      licznik++;
     }
 
-    document.getElementById("div6").innerHTML = licznik;
+    if (id == "div7") {
+      if (document.getElementById("div7").style.backgroundColor === "yellow") {
+        return;
+      }
 
-    document.getElementById("div6").style.backgroundColor = "gray";
+      document.getElementById("div7").innerHTML = licznik;
 
-    licznik++;
-  }
+      document.getElementById("div7").style.backgroundColor = "yellow";
 
-  if (id == "div7") {
-    if (document.getElementById("div7").style.backgroundColor === "gray") {
-      return;
+      licznik++;
     }
 
-    document.getElementById("div7").innerHTML = licznik;
+    if (id == "div8") {
+      if (document.getElementById("div8").style.backgroundColor === "yellow") {
+        return;
+      }
 
-    document.getElementById("div7").style.backgroundColor = "gray";
+      document.getElementById("div8").innerHTML = licznik;
 
-    licznik++;
-  }
+      document.getElementById("div8").style.backgroundColor = "yellow";
 
-  if (id == "div8") {
-    if (document.getElementById("div8").style.backgroundColor === "gray") {
-      return;
+      licznik++;
     }
 
-    document.getElementById("div8").innerHTML = licznik;
+    if (id == "div9") {
+      if (document.getElementById("div9").style.backgroundColor === "yellow") {
+        return;
+      }
 
-    document.getElementById("div8").style.backgroundColor = "gray";
+      document.getElementById("div9").innerHTML = licznik;
 
-    licznik++;
-  }
+      document.getElementById("div9").style.backgroundColor = "yellow";
 
-  if (id == "div9") {
-    if (document.getElementById("div9").style.backgroundColor === "gray") {
-      return;
+      licznik++;
     }
-
-    document.getElementById("div9").innerHTML = licznik;
-
-    document.getElementById("div9").style.backgroundColor = "gray";
-
-    licznik++;
   }
-}
